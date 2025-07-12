c85c14533a67ded46d18055caba1777aa41e520b
shreyansh0009 <saurabhsharraf@gmail.com>
2025-07-12 10:32:20 +0530

    day1

---
+++ variables.js
@@ -0,0 +1,15 @@
+// There are three types of variables in js, each with different usage.
+// i. let, ii. var, iii. const
+
+// i. var // oldest variable in js. ES1
+var a = 10;
+// can be redeclared and reassigned.
+var a = 60;
+
+// ii. let & const  // part of ES6
+const x = 69;
+// can't reassign and redeclare.{constant}
+let b = 20;
+// can't redeclared but reassignable.
+
+// note: we generally avoid using 'var' unless there is a specific reason.
