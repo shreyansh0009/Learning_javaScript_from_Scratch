859a29fac48782293da92100fcde608868a3e8b9
shreyansh0009 <saurabhsharraf@gmail.com>
2025-07-13 13:21:11 +0530

    day2

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+// i. Primitives and reference
+
+// ii. primitives: string, number, boolean, null, undefined, symbol, bigint
+
+// iii. reference: Array, objects, functions, etc.
+
+let name = "shreyansh" //string, can use "", '', & ``
+
+let a = 34;
+let b = 67.55; // number, there is no float, double in js.
+
+let isSaurabh = true; // bool
+
+// 'undefined' doesn't mean zero(0), it means you didn't assign any value to a variable(assigned by default).
+
+// 'Null' also doesn't mean zero(0), it means you intentionally assign null to a variable.
+
