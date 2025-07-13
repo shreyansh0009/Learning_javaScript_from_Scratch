1276e87d390fabd90281597313c358d6346fb740
shreyansh0009 <saurabhsharraf@gmail.com>
2025-07-13 22:08:58 +0530

    day2

--- dataTypes.js
+++ dataTypes.js
@@ -15,3 +15,28 @@ let isSaurabh = true; // bool
 
 // 'Null' also doesn't mean zero(0), it means you intentionally assign null to a variable.
 
+// bigint
+let x = 9007199254740991n;
+console.log(typeof x); //bigint.
+// the max safe intiger value in js is: 9007199254740991;
+// after that there is pricision issue for calculation that's why we use 'n' in last soo it converted to bigint.
+
+
+// Reference data types:
+// it's called reference because when we copy them, values doesn't copy, only reference will be passed.
+// if there is change in copy value then there will be change in original value.
+let z = [1, 2, 3];
+let y = z;  // reference is passed. not values.
+
+y.pop();
+console.log(z);  // [1, 2]
+// to avoid this we use spread operator to copy values."..."
+let q = [5, 6, 7, 8]
+let p = [...q];
+p.pop()
+console.log(q);  //[5, 6, 7, 8]
+console.log(p);  //[5, 6, 7]
+
+
+
+
